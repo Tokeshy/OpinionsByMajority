e6ab29a65427e30c7c035324d48cc382b7de2795
Tokeshy <64018876+Tokeshy@users.noreply.github.com>
2021-11-02 09:16:57 +0300

    Renaming
    some renaming of bad names to better

--- OpinionsByMajority.py
+++ OpinionsByMajority.py
@@ -30,42 +30,42 @@ def GetTweets(Keyword: str) -> List:
 
 
 def CleanTweets(tweets: list) -> List:
-    TweetsCleaned = []
+    tweets_cleaned = []
     for tweet in tweets:
-        TweetsCleaned.append(preprocessor.clean(str(tweet)))
+        tweets_cleaned.append(preprocessor.clean(str(tweet)))
     
-    return TweetsCleaned
+    return tweets_cleaned
 
 def GetOptions(tweets: list):
-    OpinionScore = []
+    opinion_score = []
     for tweet in tweets:
         blob = TextBlob(tweet)
-        OpinionScore.append(blob.sentiment.polarity)
+        opinion_score.append(blob.sentiment.polarity)
     
-    return OpinionScore
+    return opinion_score
 
 
 def GenereteAVGRate(Keyword: str) -> int:
     tweets = GetTweets(Keyword)
-    tweetsCleaned = CleanTweets(tweets)
-    OpinionScore = GetOptions(tweetsCleaned)
-    AVGScore = statistics.mean(OpinionScore)
+    tweets_cleaned = CleanTweets(tweets)
+    opinion_score = GetOptions(tweets_cleaned)
+    avg_score = statistics.mean(opinion_score)
 
-    return AVGScore
+    return avg_score
 
 if __name__ == '__main__':
     print('What is most popular?')
-    ItemA = input()
+    item_a = input()
     print('...or...')
-    ItemB = input()
+    item_b = input()
     print('\n')
 
-    ScoreA = GenereteAVGRate(ItemA)
-    ScoreB = GenereteAVGRate(ItemB)
+    score_a = GenereteAVGRate(item_a)
+    score_b = GenereteAVGRate(item_b)
 
-    if ScoreA > ScoreB:
-        print(f'The {ItemA} is more popular than {ItemB}... Now live w that')
-    elif ScoreA < ScoreB:
-        print(f'The {ItemB} is more popular than {ItemA}... Now live w that')
+    if score_a > score_b:
+        print(f'The {item_a} is more popular than {item_b}... Now live w that')
+    elif score_a < score_b:
+        print(f'The {item_b} is more popular than {item_a}... Now live w that')
     else:
         print('Nobody care...NOBODY...')
